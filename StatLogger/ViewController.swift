254ac799b8af5ab6610513b8087330c4a75a72e5
trinhxuanminh2000 <trinhxuanminh2000@gmail.com>
2025-06-24 17:37:06 +0700

    Initial Commit

---
+++ StatLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StatLogger
+//
+//  Created by Trịnh Xuân Minh on 24/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
